7d1d7b1ac8a2312147c57765cc6dc1064dcb11d1
George Zhang <geetransit@gmail.com>
2025-05-16 15:24:06 -0400

    Fix test script

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -2,19 +2,17 @@
 set -euxo pipefail
 ./mvnw -version
 
-cd system
-
 # Build the system/ app
 ./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q clean package liberty:create liberty:install-feature liberty:deploy
+    -q -pl system clean package liberty:create liberty:install-feature liberty:deploy
 
 # Verifies that the system app is functional
-./mvnw -ntp liberty:start
+./mvnw -ntp -pl system liberty:start
 if curl "http://localhost:9080/health" | grep "UP" ; then exit $?; fi
 if curl "http://localhost:9080/system/properties" | grep "os.name" ; then exit $?; fi
-./mvnw -ntp liberty:stop
+./mvnw -ntp -pl system liberty:stop
 
 # Delete m2 cache after completion
 rm -rf ~/.m2
