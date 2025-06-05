b3b9bc46d1eba9817cb3ef162a0bc8fe74654abe
George Zhang <geetransit@gmail.com>
2025-06-05 17:16:30 -0400

    Update to use maven wrapper (#168)
    * Update to use mvnw
    
    * Fix test script
    
    * Preserve JAVA_HOME when running tests
    
    * Remove commands for other OS since guide is only for Linux
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /finish
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump org.junit.jupiter:junit-jupiter from 5.12.2 to 5.13.0 in /start
    
    Bumps [org.junit.jupiter:junit-jupiter](https://github.com/junit-team/junit5) from 5.12.2 to 5.13.0.
    - [Release notes](https://github.com/junit-team/junit5/releases)
    - [Commits](https://github.com/junit-team/junit5/compare/r5.12.2...r5.13.0)
    
    ---
    updated-dependencies:
    - dependency-name: org.junit.jupiter:junit-jupiter
      dependency-version: 5.13.0
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Gilbert Kwan <gkwan@ca.ibm.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -euxo pipefail
-
-cd system
+./mvnw -version
 
 # Build the system/ app
-mvn -ntp -Dhttp.keepAlive=false \
+./mvnw -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q clean package liberty:create liberty:install-feature liberty:deploy
+    -q -pl system clean package liberty:create liberty:install-feature liberty:deploy
 
 # Verifies that the system app is functional
-mvn -ntp liberty:start
+./mvnw -ntp -pl system liberty:start
 if curl "http://localhost:9080/health" | grep "UP" ; then exit $?; fi
 if curl "http://localhost:9080/system/properties" | grep "os.name" ; then exit $?; fi
-mvn -ntp liberty:stop
+./mvnw -ntp -pl system liberty:stop
 
 # Delete m2 cache after completion
 rm -rf ~/.m2
